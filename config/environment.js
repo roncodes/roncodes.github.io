cd6286d67858d4170faecfd11219ad66b5afd27b
Ronald A. Richardson <me@ron.dev>
2022-12-11 16:22:43 +0700

    changed `locationType` back to auto

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function (environment) {
 		modulePrefix: 'personal',
 		environment,
 		rootURL: '/',
-		locationType: environment === 'production' ? 'hash' : 'auto',
+		locationType: 'auto',
 		EmberENV: {
 			FEATURES: {
 				// Here you can enable experimental features on an ember canary build
